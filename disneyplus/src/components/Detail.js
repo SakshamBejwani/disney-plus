32ba7eafb95e570fb56e36e6930b3bb282ab6add
Saksham Bejwani <sakshambejwani@gmail.com>
2021-05-06 23:23:26 +0530

    added buttons and logo of movie

--- disneyplus/src/components/Detail.js
+++ disneyplus/src/components/Detail.js
@@ -10,6 +10,33 @@ function Detail() {
             <ImageTitle>
                 <img src=" https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" />
             </ImageTitle>
+            <Controls>
+
+                <PlayButton>
+                    <img src = "/images/play-icon-black.png" />
+                    <span>PLAY</span>
+                </PlayButton>
+
+                <TrailerButton>
+                    <img src = "/images/play-icon-white.png" />
+                    <span>Trailer</span>
+                </TrailerButton>
+
+                <AddButton>
+                <img src = "/images/watchlist-icon.svg" />
+                </AddButton>
+
+                <GroupButton>
+                <img src = "/images/group-icon.png" />
+                </GroupButton>
+            
+            </Controls>
+            <SubTitle>
+                2018 - 7m - Family, Fantasy, Kids, Animation
+            </SubTitle>
+            <Description>
+
+            </Description>
         </Container>
     )
 }
@@ -39,11 +66,80 @@ const Background = styled.div`
 `
 
 const ImageTitle = styled.div`
+    
     height: 30vh:
+    width: 35vw;
+    min-height: 170px;
+    min-width: 200px;
     
     img {
-        width: 100%;
-        height: 100%;
+        
+        width: 25%;
+        height: 50%;
         object-fit: contain;
     }
+
+
+`
+const Controls = styled.div`
+    display: flex;
+    align-items: center;
+    
+
+
+`
+const PlayButton = styled.button`
+    border-radius: 4px;
+    font-size: 15px;
+    padding: 0px 24px;
+    margin-right: 22px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    background: rgb( 249,249,249);
+    border: none;
+    letter-spacing: 1.8px;
+    cursor: pointer;
+    &:hover {
+        background: rgb( 198,198,198);
+    }
+
+`
+const TrailerButton = styled(PlayButton)`
+    background: rgba(0, 0,0, 0.3);
+    border: 3px solid rgb(249,249,249);
+    color: rgb(249,249,249);
+    text-transform: uppercase;
+    
+
+
+`
+const AddButton = styled.button`
+    margin-right: 16px;
+    width: 44px;
+    height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    background: rgba(0, 0,0, 0.5);
+    border: 2px solid rgb(249,249,249);
+    cursor: pointer;
+    span {
+        font-size: 30px;
+        color: white;
+    }
+    &:hover {
+        background: rgb( 150,150,150);
+    }
+
+`
+const GroupButton = styled(AddButton)`
+background: rgba(0, 0,0, 0.6);
+
+`
+
+const SubTitle = styled.div`
+`
+const Description = styled.div`
 `
